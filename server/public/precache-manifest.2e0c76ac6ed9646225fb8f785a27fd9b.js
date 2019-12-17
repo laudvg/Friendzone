2504493ff678726f40df15eba619bace893baf13
Laura <laura.vivas.gamboa@gmail.com>
2019-12-17 11:16:22 +0100

    heroluuuu

---
+++ server/public/precache-manifest.2e0c76ac6ed9646225fb8f785a27fd9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd9bed98c3919f7519d06747f3304640",
+    "url": "/index.html"
+  },
+  {
+    "revision": "abcce19e6c3734e5721c",
+    "url": "/static/css/main.fb8a4741.chunk.css"
+  },
+  {
+    "revision": "be71c2f65f9381a361f4",
+    "url": "/static/js/2.01625d60.chunk.js"
+  },
+  {
+    "revision": "78ddf5a465591a03b9601ba2ddd63415",
+    "url": "/static/js/2.01625d60.chunk.js.LICENSE"
+  },
+  {
+    "revision": "abcce19e6c3734e5721c",
+    "url": "/static/js/main.ebd2b5a2.chunk.js"
+  },
+  {
+    "revision": "6b3defdcd169e5f902d5",
+    "url": "/static/js/runtime-main.1dc66141.js"
+  }
+]);
